b159ecabba1f8dacc3657f5e82bfb4bcaaab283b
Chih-Hsuan Li <122874918+kypss96134@users.noreply.github.com>
2025-09-14 12:31:24 +0200

    Delete mvdr.m

--- mvdr.m
+++
@@ -1,7 +0,0 @@
-function w = mvdr(Ns, x, a_theta_0)
-
-    R_xx = (x*x') / Ns % 參考講義 ch5 p.24 MVDR的解
-    lambda = 1/(a_theta_0'*inv(R_xx)*a_theta_0);
-
-    w = lambda * inv(R_xx) * a_theta_0;
-end
