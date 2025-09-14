138922e93e71b0d126b8e5279e9ceba845cdf7bc
Chih-Hsuan Li <122874918+kypss96134@users.noreply.github.com>
2025-09-14 12:33:26 +0200

    Delete msinr.m

--- msinr.m
+++
@@ -1,7 +0,0 @@
-function w = msinr(Ns, x, a_theta_0, sp)
-    x_k = x - (a_theta_0*sp);
-    R_in = (x_k * x_k') / Ns ;
-
-    lambda = 1/(a_theta_0' * inv(R_in) * a_theta_0);
-    w = lambda * inv(R_in) * a_theta_0;
-end
